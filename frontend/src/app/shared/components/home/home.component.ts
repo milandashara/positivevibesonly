70be58ef461756e39629c6dc6498d634aa34fff5
milan <milandashara@gmail.com>
2020-07-22 16:27:49 +0800

    pagination in angular and change context path

--- frontend/src/app/shared/components/home/home.component.ts
+++ frontend/src/app/shared/components/home/home.component.ts
@@ -17,7 +17,6 @@ export class HomeComponent implements OnInit {
   menuItems: MenuItem[];
   selectedArticle: Article;
   selectedCategory: string;
-  articles: Article[]
 
   constructor(private menuService: MenuService, private route: ActivatedRoute,private articleService:ArticleService) {
     this.appName = environment.appName;
@@ -31,10 +30,6 @@ export class HomeComponent implements OnInit {
 
     // this.selectedArticle = {id: '1', header: 'Article name', text: 'article text', author: 'author'};
     this.selectedCategory = this.route.snapshot.data.category;
-
-    this.articleService.getArticle(this.selectedCategory).subscribe(data => {
-      this.articles = data.articles;
-    })
   }
 
 }
